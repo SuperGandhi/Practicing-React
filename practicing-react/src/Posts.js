b6bb7aed7303ab43acf0cb847a68ebd4b9d2ad5f
SuperGandhi <patrickborjabueno@hotmail.com>
2022-10-18 12:02:23 -0500

    Use library reacticons

--- practicing-react/src/Posts.js
+++ practicing-react/src/Posts.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import {VscBug, VscGlobe} from 'react-icons/vsc';
+
 
 const Posts = () => {
   return (
-    <button onClick={()=>{
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error))
-    }}>
-      Get data
-    </button>
+    <>
+      <button onClick={()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch(error => console.log(error))
+      }}>
+        <VscBug/>
+        <VscGlobe/>
+        Get data
+      </button>
+    </>
   )
 }
 
